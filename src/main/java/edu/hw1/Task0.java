3677d4d4f809175590159aabd8629f29b213611a
lloppy <ankudinovapol@gmail.com>
2023-10-11 14:22:59 +0500

    refactor hw1 vers3

--- src/main/java/edu/hw1/Task0.java
+++ src/main/java/edu/hw1/Task0.java
@@ -3,11 +3,12 @@ package edu.hw1;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-public class Task0 {
-    private final static Logger LOGGER = LogManager.getLogger();
+public final class Task0 {
+    private Task0() {
+    }
+    private static final Logger LOGGER = LogManager.getLogger();
 
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         LOGGER.info("Привет, мир!");
-
     }
 }
--- src/main/java/edu/hw1/Task1.java
+++ src/main/java/edu/hw1/Task1.java
@@ -2,6 +2,7 @@ package edu.hw1;
 
 public final class Task1 {
     private static final int SECONDS_PER_MINUTE = 60;
+    private static final String DIGIT_PATTERN = "\\d+";
 
     private Task1() {
     }
@@ -9,10 +10,11 @@ public final class Task1 {
     public static int minutesToSeconds(final String stringTime) {
         String[] time = stringTime.split(":");
 
-        if (time.length != 2 || !time[0].matches("\\d+") || !time[1].matches("\\d+")) {
+        if (time.length != 2
+            || !time[0].matches(DIGIT_PATTERN)
+            || !time[1].matches(DIGIT_PATTERN)) {
             return -1;
         }
-
         int minutes = Integer.parseInt(time[0]);
         int seconds = Integer.parseInt(time[1]);
 
