6ee8fb8c835505403d41f082dc5856f30c04b75b
lloppy <ankudinovapol@gmail.com>
2023-10-11 14:26:29 +0500

    refactor hw1 vers3

--- src/main/java/edu/hw1/Task0.java
+++ src/main/java/edu/hw1/Task0.java
@@ -6,9 +6,12 @@ import org.apache.logging.log4j.Logger;
 public final class Task0 {
     private Task0() {
     }
+
     private static final Logger LOGGER = LogManager.getLogger();
 
     public static void main(final String[] args) {
+        // Метод выводит в лог - Привет, мир!.
         LOGGER.info("Привет, мир!");
     }
+
 }
