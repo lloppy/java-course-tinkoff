76e7de64c97c1e647fb78905bff67791dcc576de
lloppy <ankudinovapol@gmail.com>
2023-11-02 00:27:13 +0500

    refactor animal class for checkstyle

--- src/main/java/edu/streams/Animal.java
+++ src/main/java/edu/streams/Animal.java
@@ -17,12 +17,17 @@ public record Animal(
         M, F
     }
 
+    private static final int PAWS_CAT_DOG = 4;
+    private static final int PAWS_BIRD = 2;
+    private static final int PAWS_FISH = 0;
+    private static final int PAWS_SPIDER = 8;
+
     public int paws() {
         return switch (type) {
-            case CAT, DOG -> 4;
-            case BIRD -> 2;
-            case FISH -> 0;
-            case SPIDER -> 8;
+            case CAT, DOG -> PAWS_CAT_DOG;
+            case BIRD -> PAWS_BIRD;
+            case FISH -> PAWS_FISH;
+            case SPIDER -> PAWS_SPIDER;
         };
     }
 }
