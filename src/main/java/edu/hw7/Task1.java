eb8914d67151b6941b4b8aa9c8256aa8b733e0ee
lloppy <ankudinovapol@gmail.com>
2023-11-22 22:33:50 +0500

    solve 1, 2 tasks, add 3 task

---
+++ src/main/java/edu/hw7/Task1.java
@@ -0,0 +1,59 @@
+package edu.hw7;
+
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.logging.Logger;
+
+public final class Task1 {
+    private Task1() {
+    }
+
+    private static final Logger LOGGER = Logger.getLogger(Task1.class.getName());
+
+    public static Integer getThreadsResult(final Integer limit) {
+        Action action = new Action(limit);
+
+        Thread incrementer = action.getNewThread("First");
+        Thread incrementer2 = action.getNewThread("Second");
+        Thread incrementer3 = action.getNewThread("Third");
+
+        incrementer.start();
+        incrementer2.start();
+        incrementer3.start();
+        try {
+            incrementer.join();
+            incrementer2.join();
+            incrementer3.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            System.out.println(action.getValue());
+        }
+        return action.getValue();
+    }
+
+    private static final class Action {
+        private AtomicInteger value = new AtomicInteger(0);
+        private int limit;
+
+        Action(final int limitValueForOneThread) {
+            this.limit = limitValueForOneThread;
+        }
+
+        public Thread getNewThread(final String nameThread) {
+            var incrementer = new Thread(() -> {
+                for (int i = 0; i < this.limit; i++) {
+                    increment();
+                    LOGGER.info(nameThread + " thread increased counter to " + value);
+                }
+            });
+            return incrementer;
+        }
+
+        private void increment() {
+            value.incrementAndGet();
+        }
+
+        public int getValue() {
+            return value.get();
+        }
+    }
+}
---
+++ src/main/java/edu/hw7/Task2.java
@@ -0,0 +1,23 @@
+package edu.hw7;
+
+import java.math.BigInteger;
+import java.util.stream.LongStream;
+
+public final class Task2 {
+    private Task2() {
+    }
+
+    public static BigInteger getFactorial(final long n) {
+        if (n < 0) {
+            throw new IllegalArgumentException("Your number is negative!");
+        }
+        return LongStream
+            .rangeClosed(1, n)
+            .parallel()
+            .mapToObj(BigInteger::valueOf)
+            .reduce(
+                BigInteger.ONE,
+                BigInteger::multiply
+            );
+    }
+}
