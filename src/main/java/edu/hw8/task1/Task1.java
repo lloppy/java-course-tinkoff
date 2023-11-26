08ad31410d5a4c19ae63d70a429663fe50178a47
lloppy <ankudinovapol@gmail.com>
2023-11-26 21:25:36 +0500

    init

--- src/main/java/edu/hw8/task1/Task1.java
+++ src/main/java/edu/hw8/task1/Task1.java
@@ -1,35 +1,7 @@
 package edu.hw8.task1;
 
-import java.net.Socket;
-
 public class Task1 {
-    boolean needShutDonw = false;
-
     private Task1() {
     }
 
-    void fun() {
-        Socket socket = new Socket();
-
-        Thread serverThread = new Thread(() -> {
-            while (needShutDonw) {
-                serverThread.interrupt();
-            }
-
-        } catch(InterruptedException e){
-            // Обработка InterruptedException
-            // Второй вызов interrupt() для передачи сигнала обработки прерывания
-            Thread.currentThread().interrupt();
-        }
-    });
-
-    Thread stopperThread = new Thread(() -> {
-        serverThread.interrupt();
-    });
-
-// Запуск потоков
-        stopperThread.start();
-        serverThread.start();
-
-}
 }
