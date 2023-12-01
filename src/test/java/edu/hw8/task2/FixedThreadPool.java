ba90c764ebbd634b31c72a11ec9d1b13d4dfbcf9
lloppy <ankudinovapol@gmail.com>
2023-12-01 12:32:09 +0500

    init task2

---
+++ src/test/java/edu/hw8/task2/FixedThreadPool.java
@@ -0,0 +1,18 @@
+package edu.hw8.task2;
+
+public class FixedThreadPool implements ThreadPool{
+    @Override
+    public void start() {
+
+    }
+
+    @Override
+    public void execute(Runnable runnable) {
+
+    }
+
+    @Override
+    public void close() throws Exception {
+
+    }
+}
