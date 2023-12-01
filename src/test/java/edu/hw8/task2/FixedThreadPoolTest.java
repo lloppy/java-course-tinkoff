05222f8e54450d5641c4e6361570b55db0b9ff26
lloppy <ankudinovapol@gmail.com>
2023-12-02 00:28:39 +0500

    refactor

--- src/test/java/edu/hw8/task2/FixedThreadPoolTest.java
+++ src/test/java/edu/hw8/task2/FixedThreadPoolTest.java
@@ -1,11 +1,13 @@
 package edu.hw8.task2;
 
 import java.util.concurrent.CountDownLatch;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class FixedThreadPoolTest {
+    @RepeatedTest(10)
     @Test
     public void testThreadPoolExecution() throws Exception {
         int threadsNumber = 3;
@@ -24,7 +26,6 @@ class FixedThreadPoolTest {
                 }
             });
         }
-        assertTrue(1 <= latch.getCount());
         assertTrue(3 == latch.getCount());
 
         latch.await();
