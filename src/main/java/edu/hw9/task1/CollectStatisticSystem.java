8730ca6a885aca5bd44102455e4e9d2ba24686ae
lloppy <ankudinovapol@gmail.com>
2023-12-05 21:19:50 +0500

    add task1

---
+++ src/main/java/edu/hw9/task1/CollectStatisticSystem.java
@@ -0,0 +1,52 @@
+package edu.hw9.task1;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+public final class CollectStatisticSystem {
+    public CollectStatisticSystem() {
+    }
+
+    private final ConcurrentLinkedQueue<Metric> metrics = new ConcurrentLinkedQueue<>();
+
+    public void push(final String metricName, final double[] values) {
+        metrics.add(new Metric(metricName, values));
+    }
+
+    public List<MetricType> stats() {
+        Map<String, List<Double>> metricsData = new ConcurrentHashMap<>();
+
+        metrics.forEach(metric -> {
+            metricsData.compute(metric.metricName(), (key, existingValues) -> {
+                if (existingValues == null) {
+                    existingValues = new ArrayList<>();
+                }
+                for (double value : metric.values()) {
+                    existingValues.add(value);
+                }
+                return existingValues;
+            });
+        });
+
+        List<MetricType> result = new ArrayList<>();
+        metricsData.forEach((metricName, values) -> {
+            double sum = 0;
+            double min = Double.MAX_VALUE;
+            double max = Double.MIN_VALUE;
+
+            for (double value : values) {
+                sum += value;
+                min = Math.min(min, value);
+                max = Math.max(max, value);
+            }
+
+            double average = sum / values.size();
+            result.add(new MetricType(metricName, sum, average, min, max));
+        });
+
+        return result;
+    }
+}
