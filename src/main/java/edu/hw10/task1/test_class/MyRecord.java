b3d0fd68fb6211f4d3a781144fdd855a7fa66cc2
lloppy <ankudinovapol@gmail.com>
2023-12-17 15:29:44 +0500

    add myClass in task1, add some tests

--- src/main/java/edu/hw10/task1/test_class/MyRecord.java
+++ src/main/java/edu/hw10/task1/test_class/MyRecord.java
@@ -5,7 +5,7 @@ import edu.hw10.task1.annotations.Min;
 import edu.hw10.task1.annotations.NotNull;
 
 public record MyRecord(@Max(10) @Min(9) int intValue, @Max(1) double doubleValue, @NotNull String string) {
-    public static MyRecord create(@Max(10) @Min(9) int intValue, @Max(1) double doubleValue, @NotNull String string) {
+    public static MyRecord createMyRecord(@Max(10) @Min(9) int intValue, @Max(1) double doubleValue, @NotNull String string) {
         return new MyRecord(intValue, doubleValue, string);
     }
 }
--- src/test/java/edu/hw10/task1/RandomObjectGeneratorTest.java
+++ src/test/java/edu/hw10/task1/RandomObjectGeneratorTest.java
@@ -1,6 +1,7 @@
 package edu.hw10.task1;
 
 import edu.hw10.task1.generator.RandomObjectGenerator;
+import edu.hw10.task1.test_class.MyClass;
 import edu.hw10.task1.test_class.MyRecord;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -29,12 +30,42 @@ public class RandomObjectGeneratorTest {
         RandomObjectGenerator generator = new RandomObjectGenerator(MyRecord.class);
 
         // When
-        MyRecord myRecordInstance = generator.nextObject("create");
+        MyRecord myRecordInstance = generator.nextObject("createMyRecord");
 
         // Then
         assertNotNull(myRecordInstance);
         assertEquals(9, myRecordInstance.intValue());
         assertTrue(myRecordInstance.doubleValue() < 1);
         assertNotNull(myRecordInstance.string());
+    }
+
+    @Test
+    public void testGenerateMyClass() {
+        // Given
+        RandomObjectGenerator generator = new RandomObjectGenerator(MyClass.class);
+
+        // When
+        MyClass myClassInstance = generator.nextObject();
+
+        // Then
+        assertNotNull(myClassInstance);
+        assertEquals(9, myClassInstance.getIntValue());
+        assertTrue(myClassInstance.getDoubleValue() < 1);
+        assertNotNull(myClassInstance.getStringValue());
+    }
+
+    @Test
+    public void testGenerateMyClassWithFactoryMethod() {
+        // Given
+        RandomObjectGenerator generator = new RandomObjectGenerator(MyClass.class);
+
+        // When
+        MyClass myClassInstance = generator.nextObject("createMyClass");
+
+        // Then
+        assertNotNull(myClassInstance);
+        assertEquals(9, myClassInstance.getIntValue());
+        assertTrue(myClassInstance.getDoubleValue() < 1);
+        assertNotNull(myClassInstance.getStringValue());
     }
 }
